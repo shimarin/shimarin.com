066eddcdc1f5f320dc596196578dc90aa7e6f75f
Tomoatsu Shimada <shimada@walbrix.com>
2014-08-16 16:54:55 +0900

    angulartics adopted

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-angular.module("MyApp", ["ui.bootstrap"])
+angular.module("MyApp", ["ui.bootstrap", "angulartics", "angulartics.google.analytics"])
 .controller("NetKjkController", ["$scope", "$modal", function($scope, $modal) {
     $scope.about = function() {
         $modal.open({templateUrl:"about.html"});
     }
+}])
+.controller("OptoutController", ["$scope", function($scope) {
+    var _gaq = _gaq || [];
+    console.log(_gaq);
+    _gaq.push(['_setVar', 'no_analytics']);
 }])
 .run(["$rootScope", function($scope) {
     $scope.message = "Hello! World!";
